a3dfafedabfaf33624d557cc8d7435c4882ff213
Derrik Gratz <derrik.gratz@gmail.com>
2024-01-02 17:14:02 -0500

    save image

--- Immunology/celltype-heirarchy.R
+++ Immunology/celltype-heirarchy.R
@@ -1,14 +1,12 @@
 library(yaml)
 library(data.tree)
 library(DiagrammeR)
-# library(here)
+library(here)
+library(rsvg)
 
-# here::i_am('Immunology/celltype-heirarchy.R')
-
-data <- yaml::read_yaml('celltype-heirarchy.yaml')
+data <- yaml::read_yaml(here('Immunology/celltype-heirarchy.yaml'))
 
 osNode <- data.tree::as.Node(data, interpretNullAsList = TRUE)
 
-pdf(file = 'celltype-heirarchy.pdf', width = 10, height = 10)
-plot(osNode)
-dev.off()
+export_graph(ToDiagrammeRGraph(osNode), "Immunology/celltype-heirarchy.pdf")
+
